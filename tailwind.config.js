ee7be186a8d12ff23a93a32bbc445270aad8e355
Peter Parker <web3slinger@gmail.com>
2022-05-12 14:48:27 -0500

    Build index page and add theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,92 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  darkMode: 'class',
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.900'),
+            a: {
+              color: theme('colors.blue.700'),
+              '&:hover': {
+                color: theme('colors.blue.500'),
+              },
+            },
+            'code::before': {
+              content: '""',
+            },
+            'code::after': {
+              content: '""',
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+            a: {
+              color: theme('colors.purple.500'),
+              '&:hover': {
+                color: theme('colors.purple.400'),
+              },
+            },
+
+            h1: {
+              color: theme('colors.gray.100'),
+            },
+            h2: {
+              color: theme('colors.gray.100'),
+            },
+            h3: {
+              color: theme('colors.gray.100'),
+            },
+            h4: {
+              color: theme('colors.gray.100'),
+            },
+            h5: {
+              color: theme('colors.gray.100'),
+            },
+            h6: {
+              color: theme('colors.gray.100'),
+            },
+
+            strong: {
+              color: theme('colors.gray.100'),
+            },
+
+            code: {
+              color: theme('colors.gray.300'),
+            },
+
+            figcaption: {
+              color: theme('colors.gray.500'),
+            },
+          },
+        },
+      }),
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      scale: {
+        102.5: '1.025',
+      },
+      colors: {
+        base: '#0f0f0f',
+      },
+      screens: {
+        xs: '481px',
+      },
+      fontSize: {
+        xxs: '0.7rem',
+      },
+    },
   },
-  plugins: [],
+  variants: {
+    typography: ['dark'],
+  },
+  plugins: [require('@tailwindcss/typography')],
 }
