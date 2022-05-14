3eb1024921464febbb4ff98594a30211d2fcb8ea
Peter Parker <web3slinger@gmail.com>
2022-05-13 19:09:44 -0500

    Add project section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useMintCountContext } from '../context/ContextProvider'
+import { FiExternalLink } from 'react-icons/fi'
+import { GiSwordwoman, GiDiamondsSmile, GiHammerNails, GiSpiderWeb } from 'react-icons/gi'
 import FadeIn from 'react-fade-in'
 import Router from 'next/router'
 import Image from 'next/image'
@@ -8,6 +10,7 @@ import { Navbar } from '../components/Nav/Navbar'
 
 const Home = () => {
   const [mintCount] = useMintCountContext()
+  const [selected, setSelected] = useState('projects')
 
   useEffect(() => {
     if (mintCount === 0) {
@@ -26,17 +29,153 @@ const Home = () => {
                 <Image src="/img/smol-spider.png" alt="Smol Spider-Man" width={500} height={500} />
               </div>
               <div className="grid w-full grid-cols-2 gap-2 mb-6 text-sm font-medium">
-                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">Solidity</div>
-                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">JavaScript/TypeScript</div>
-                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">React/Next.js</div>
-                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">Tailwind CSS</div>
-                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">Node.js</div>
-                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">Python</div>
+                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">
+                  Solidity
+                </div>
+                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">
+                  JavaScript/TypeScript
+                </div>
+                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">
+                  React/Next.js
+                </div>
+                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">
+                  Tailwind CSS
+                </div>
+                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">
+                  Node.js
+                </div>
+                <div className="px-3 py-6 border border-gray-300 bg-gray-50 rounded-xl dark:bg-black dark:border-gray-800">
+                  Python
+                </div>
               </div>
             </div>
             <div className="relative flex flex-col items-start flex-shrink w-full min-w-0 ml-0 lg:ml-6">
-              <h1 className="text-4xl font-semibold">Patrick Torralba</h1>
-              <p className="text-2xl text-gray-500">Buildooor.</p>
+              <div className="flex flex-col mb-6">
+                <h1 className="mb-2 text-4xl font-bold">Patrick Torralba</h1>
+                <p className="text-2xl font-medium text-gray-500">Buildooor.</p>
+              </div>
+              <div className="flex flex-col w-full mb-6">
+                <ul className="flex gap-2 mb-6">
+                  <li
+                    className={
+                      selected === 'projects'
+                        ? 'px-4 py-1 text-sm font-semibold text-white bg-gray-700 transition-all rounded-full cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-600 dark:bg-gray-300 hover:text-gray-500 dark:text-gray-800'
+                        : 'px-4 py-1 text-sm font-semibold text-gray-500 transition-all rounded-full cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-600 dark:bg-black dark:text-gray-300'
+                    }
+                    onClick={() => setSelected('projects')}
+                  >
+                    Projects
+                  </li>
+                  <li
+                    className={
+                      selected === 'info'
+                        ? 'px-4 py-1 text-sm font-semibold text-white bg-gray-700 transition-all rounded-full cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-600 dark:bg-gray-300 hover:text-gray-500 dark:text-gray-800'
+                        : 'px-4 py-1 text-sm font-semibold text-gray-500 transition-all rounded-full cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-600 dark:bg-black dark:text-gray-300'
+                    }
+                    onClick={() => setSelected('info')}
+                  >
+                    Info
+                  </li>
+                  <li
+                    className={
+                      selected === 'about'
+                        ? 'px-4 py-1 text-sm font-semibold text-white bg-gray-700 transition-all rounded-full cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-600 dark:bg-gray-300 hover:text-gray-500 dark:text-gray-800'
+                        : 'px-4 py-1 text-sm font-semibold text-gray-500 transition-all rounded-full cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-600 dark:bg-black dark:text-gray-300'
+                    }
+                    onClick={() => setSelected('about')}
+                  >
+                    Activity
+                  </li>
+                </ul>
+                {selected === 'projects' ? (
+                  <div className="grid gap-6">
+                    <a
+                      className="flex flex-col sm:flex-row text-center sm:text-left px-8 sm:px-6 py-6 rounded-md border border-gray-300 dark:border-gray-800 max-w-2xl cursor-pointer focus:outline-none transition duration-300 ease-in-out transform hover:scale-102.5"
+                      href="#"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <div className="flex items-center justify-center w-full mb-4 mr-6 text-4xl sm:w-1/12 sm:mb-0">
+                        <div>
+                          <GiSwordwoman />
+                        </div>
+                      </div>
+                      <div className="flex flex-col items-center w-full sm:items-start sm:w-11/12">
+                        <div className="flex items-center gap-2">
+                          <div className="font-semibold">Metaverse Slayer</div>
+                          <FiExternalLink />
+                        </div>
+                        <div className="text-sm text-gray-600 dark:text-gray-300">
+                          Turn-based NFT browser game.
+                        </div>
+                      </div>
+                    </a>
+                    <a
+                      className="flex flex-col sm:flex-row text-center sm:text-left px-8 sm:px-6 py-6 rounded-md border border-gray-300 dark:border-gray-800 max-w-2xl cursor-pointer focus:outline-none transition duration-300 ease-in-out transform hover:scale-102.5"
+                      href="#"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <div className="flex items-center justify-center w-full mb-4 mr-6 text-4xl sm:w-1/12 sm:mb-0">
+                        <div>
+                          <GiDiamondsSmile />
+                        </div>
+                      </div>
+                      <div className="flex flex-col items-center w-full sm:items-start sm:w-11/12">
+                        <div className="flex items-center gap-2">
+                          <div className="font-semibold">Epic NFTs</div>
+                          <FiExternalLink />
+                        </div>
+                        <div className="text-sm text-gray-600 dark:text-gray-300">
+                          3 word epic NFTs that makes you laugh.
+                        </div>
+                      </div>
+                    </a>
+                    <a
+                      className="flex flex-col sm:flex-row text-center sm:text-left px-8 sm:px-6 py-6 rounded-md border border-gray-300 dark:border-gray-800 max-w-2xl cursor-pointer focus:outline-none transition duration-300 ease-in-out transform hover:scale-102.5"
+                      href="#"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <div className="flex items-center justify-center w-full mb-4 mr-6 text-4xl sm:w-1/12 sm:mb-0">
+                        <div>
+                          <GiHammerNails />
+                        </div>
+                      </div>
+                      <div className="flex flex-col items-center w-full sm:items-start sm:w-11/12">
+                        <div className="flex items-center gap-2">
+                          <div className="font-semibold">Daedalus DAO</div>
+                          <FiExternalLink />
+                        </div>
+                        <div className="text-sm text-gray-600 dark:text-gray-300">
+                          Community for builders using thirdweb.
+                        </div>
+                      </div>
+                    </a>
+                    <a
+                      className="flex flex-col sm:flex-row text-center sm:text-left px-8 sm:px-6 py-6 rounded-md border border-gray-300 dark:border-gray-800 max-w-2xl cursor-pointer focus:outline-none transition duration-300 ease-in-out transform hover:scale-102.5"
+                      href="#"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <div className="flex items-center justify-center w-full mb-4 mr-6 text-4xl sm:w-1/12 sm:mb-0">
+                        <div>
+                          <GiSpiderWeb />
+                        </div>
+                      </div>
+                      <div className="flex flex-col items-center w-full sm:items-start sm:w-11/12">
+                        <div className="flex items-center gap-2">
+                          <div className="font-semibold">web3slinger.dev</div>
+                          <FiExternalLink />
+                        </div>
+                        <div className="text-sm text-gray-600 dark:text-gray-300">
+                          The website you're currently on! Made with Next.js and Tailwind CSS.
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                ) : null}
+              </div>
             </div>
           </div>
         </div>
