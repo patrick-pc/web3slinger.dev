f5f31740cd893d64640c8d8823ae4f6ae7969a03
Peter Parker <web3slinger@gmail.com>
2022-05-22 17:32:16 -0500

    Add mint loader

--- pages/mint.js
+++ pages/mint.js
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import { useTheme } from 'next-themes'
 import { useMintCountContext } from '../context/ContextProvider'
+import { GenericMeta } from '../components/GenericMeta'
+import { Orbit } from '@uiball/loaders'
 import FadeIn from 'react-fade-in'
 import NextLink from 'next/link'
-import { GenericMeta } from '../components/GenericMeta'
 
 const Mint = () => {
+  const { theme } = useTheme()
   const [mintCount, setMintCount] = useMintCountContext()
   const [isMinting, setIsMinting] = useState(false)
 
@@ -13,7 +16,7 @@ const Mint = () => {
     setTimeout(() => {
       setMintCount(1)
       setIsMinting(false)
-    }, 2000)
+    }, 1500)
   }
 
   return (
@@ -21,20 +24,26 @@ const Mint = () => {
       <GenericMeta title="web3slinger.dev | mint" description="Mint web3slinger." />
 
       <FadeIn className="flex flex-col items-center justify-center w-screen h-screen">
-        <h1 className="mb-2 text-4xl font-medium">gm ☕️</h1>
+        <h1 className="mb-2 text-4xl font-medium">Gm ☕️</h1>
         <h2 className="mb-2 text-lg">{mintCount}/1</h2>
         {mintCount === 0 ? (
-          <button
-            className="px-8 py-2 transition duration-300 ease-in-out border border-gray-500 rounded hover:scale-110"
-            onClick={mint}
-            disabled={isMinting}
-          >
-            {isMinting ? 'Minting...' : 'Mint'}
-          </button>
+          isMinting ? (
+            <div className="px-8 py-1">
+              <Orbit size={34} color={theme === 'dark' ? '#ffffff' : '#000000'} />
+            </div>
+          ) : (
+            <button
+              className="px-8 py-2 border border-gray-300 dark:border-gray-800 rounded-md focus:outline-none transition duration-300 ease-in-out transform hover:scale-110 w-36"
+              onClick={mint}
+              disabled={isMinting}
+            >
+              Mint
+            </button>
+          )
         ) : (
           <NextLink href="/">
             <button
-              className="px-8 py-2 transition duration-300 ease-in-out border border-gray-500 rounded hover:scale-110"
+              className="px-8 py-2 border border-gray-300 dark:border-gray-800 rounded-md focus:outline-none transition duration-300 ease-in-out transform hover:scale-110 w-36"
               onClick={mint}
               disabled={isMinting}
             >
